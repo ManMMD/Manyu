86d8eb356fc9a0dc0c212e6ce8f1865dd5770f63
root <root@localhost.localdomain>
2018-10-12 09:05:13 +0800

    add day07 files

--- homework_day_7/12.py
+++ homework_day_7/12.py
@@ -1,6 +1,5 @@
 def isConsecutiveFour(values):
     flag = 0
-    print(len(values))
     if len(values)>=4:
         for i in range(len(values)-3):
             if values[i]==values[i+1] and values[i]==values[i+2] and values[i]==values[i+3]:
@@ -19,7 +18,7 @@ def isConsecutiveFour(values):
 
 if __name__ == '__main__':
     a=raw_input('>>')
-    s=a.split()
+    s=list(str(a))
     for i in range(len(s)):
         s[i]=int(s[i])
 isConsecutiveFour(s)
