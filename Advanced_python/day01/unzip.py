01de208e1378f94e3cf80fee60939a71a30f10c1
root <root@localhost.localdomain>
2018-10-12 17:17:11 +0800

    day01

---
+++ Advanced_python/day01/unzip.py
@@ -0,0 +1,36 @@
+import zipfile
+import optparse
+
+from threading import Thread
+
+def extractFile(zfile,password):
+    try:
+        zFile.extractall(pwd=password)
+        print('[+] Found password:'+ password+'\n')
+    except:
+        pass
+
+def main():
+    parser =optparse.OptionParser('usage%prog'+'-f <zipfile> -d <dictionary>')
+    parser.add_option('-f',dest = 'zname',type='string',help='specify zip file')
+    parser.add_option('-d',dest = 'dname',type='string',help='specify zip file')
+    (options,args) = parser.parse_args()
+
+    if (options.zname == None) or (options.dname == None):
+        print parser.usage
+        exit(0)
+
+    else:
+        zname = options.zname
+        dname = options.dname
+        zFile = zipfile.ZipFile(zname)
+        passFile = open(dname)
+        for line in passFile.readlines():
+            password = line.strip('\n')
+            t = Thread(target=extractFile,args=(zFile,password))
+            t.start()
+            print  Thread.getName(t) + 'is start'
+
+if __name__ == '__main__':
+    main()
+
---
+++ Advanced_python/day01/unzip_test.py
@@ -0,0 +1,22 @@
+import zipfile
+import time
+from threading import Thread
+
+def extractFile(password):
+    path='/root/Python/Manyu/data/wen.zip'
+    zFile=zipfile.ZipFile(path)
+    try:
+        zFile.extractall(pwd=password)
+        print('[+] Found password:'+ password +'\n')
+    except Exception as e:
+       pass
+
+def main():
+    passFile = open('/root/Python/Manyu/data/threepassword.txt','r')
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        extractFile(password)
+
+if __name__ == '__main__':
+    main()
+
